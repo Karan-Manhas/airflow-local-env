d16714123ec0b2a900c9915d6ce5eff66e0778af
kmanhas <km2354@bath.ac.uk>
2024-03-21 22:45:45 +0000

    revert back to prev

--- dags/scrape_data.py
+++ dags/scrape_data.py
@@ -6,9 +6,12 @@ def scrape_data():
     import requests
     from bs4 import BeautifulSoup
 
-    url = "https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=grade+10+pokemon+card&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=grade+10+pokemon+card&_osacat=0&LH_BIN=1&_ipg=240&_pgn=2"  # URL for the first page only
-    soup = get_data(url)
-    scraped_data = parse(soup)  # Scrape data from the single page
+    scraped_data = []  # Initialize an empty list to store data
+
+    for page_number in enumerate (range(2, 12)):
+        url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=grade+10+pokemon+card&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=grade+10+pokemon+card&_osacat=0&LH_BIN=1&_ipg=240&_pgn={page_number}"  # Your original URL here
+        soup = get_data(url)
+        scraped_data.extend(parse(soup))  # Add parsed data from each page
 
     # You can now use the scraped_data list for further processing
     # (e.g., print it, store it in a file, pass it to another task)
