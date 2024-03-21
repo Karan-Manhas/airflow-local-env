4536b1729fc7b0ab18d11f78896c8856a3bd385b
kmanhas <km2354@bath.ac.uk>
2024-03-21 22:50:59 +0000

    add in save_to_csv

--- dags/scrape_data.py
+++ dags/scrape_data.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
+import csv
 
 def scrape_data():
     import requests
@@ -8,14 +9,22 @@ def scrape_data():
 
     scraped_data = []  # Initialize an empty list to store data
 
-    for page_number in enumerate (range(2, 12)):
+    for page_number in range(2, 12):
         url = f"https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=grade+10+pokemon+card&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=grade+10+pokemon+card&_osacat=0&LH_BIN=1&_ipg=240&_pgn={page_number}"  # Your original URL here
         soup = get_data(url)
         scraped_data.extend(parse(soup))  # Add parsed data from each page
 
-    # You can now use the scraped_data list for further processing
-    # (e.g., print it, store it in a file, pass it to another task)
-    # print(scraped_data)  # Example printing
+    # Save data to CSV file
+    save_to_csv(scraped_data)
+
+def save_to_csv(data):
+    with open("C:\Users\Karan\DataEngineeringProject1\csv-files", "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["name", "price", "link", "postage_cost"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for item in data:
+            writer.writerow(item)
 
 # Get raw data function (unchanged)
 def get_data(url):
